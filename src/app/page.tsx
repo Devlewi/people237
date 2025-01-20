a9c1bba89da71e54adaec9ffa2c3fe536ed974cc
devlewi <lewimarcjean3@gmail.com>
2025-01-20 17:16:15 +0000

    update 12

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,19 @@ export const metadata: Metadata = {
     icon: "/favicon.ico",
     apple: "/apple-touch-icon.png",
   },
-  
+  openGraph: {
+    title: "ACCUEIL | PEOPLE 237",
+    description: "Page d'accueil de PEOPLE 237",
+    url: "https://people237.vercel.app/",
+    siteName: "PEOPLE 237",
+    images: [{ url: "/favicon.ico", width: 120, height: 120 }],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "ACCUEIL | PEOPLE 237",
+    description: "Page d'accueil de PEOPLE 237",
+    images: ["/favicon.ico"],
+  },
 
 };
 
