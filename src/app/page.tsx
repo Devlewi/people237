ef8cf4247685e688b904e9b7247652a71a26bb21
devlewi <lewimarcjean3@gmail.com>
2025-01-20 17:14:24 +0000

    update 10

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,25 +5,7 @@ import HomePageContentComponent from "./components/HomePageContentComponent";
 
 
 export const metadata: Metadata = {
-  title: "ACCUEIL | PEOPLE 237",
-  description: "Page d'accueil de PEOPLE 237, découvrez nos dernières actualités.",
-  icons: {
-    icon: "/favicon.ico",
-    apple: "/apple-touch-icon.png",
-  },
-  openGraph: {
-    title: "ACCUEIL | PEOPLE 237",
-    description: "Page d'accueil de PEOPLE 237",
-    url: "https://people237.vercel.app/",
-    siteName: "PEOPLE 237",
-    images: [{ url: "/favicon.ico", width: 120, height: 120 }],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "ACCUEIL | PEOPLE 237",
-    description: "Page d'accueil de PEOPLE 237",
-    images: ["/favicon.ico"],
-  },
+  
 
 };
 
