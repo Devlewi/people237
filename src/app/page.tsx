368e1e33102658e3a6c680810e15636417729b65
devlewi <lewimarcjean3@gmail.com>
2025-01-20 17:24:38 +0000

    update 16

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,13 @@ import HomePageContentComponent from "./components/HomePageContentComponent";
 
 
 export const metadata: Metadata = {
-  title: "ACCUEIL | PEOPLE 237",
-  description: "Page d'accueil de PEOPLE 237, découvrez nos dernières actualités.",
+  title: "PAGE ACCUEIL | PEOPLE 237",
+  description: "Page Accueil PEOPLE 237, découvrez nos dernières actualités.",
   icons: {
     icon: "/favicon.ico",
     apple: "/apple-touch-icon.png",
-  },/*
+  },
+  /*
   openGraph: {
     title: "ACCUEIL | PEOPLE 237",
     description: "Page d'accueil de PEOPLE 237",
