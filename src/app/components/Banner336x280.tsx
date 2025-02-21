9a16423bc31356092fd24afdc952da10e88a80a4
devlewi <lewimarcjean3@gmail.com>
2025-02-21 16:57:46 +0000

    udapte encart 2

--- src/app/components/Banner336x280.tsx
+++ src/app/components/Banner336x280.tsx
@@ -10,21 +10,26 @@ const Banner336x280: React.FC = () => {
   }, []);
 
   if (!isClient) return null; // Empêche le rendu côté serveur
-
+//text-align:center;background:#eae8e8;margin-top:0px;
   return (
-    <div>
+    <div className='col-md-12 justify-content-center' style={{textAlign:'center', background:'#eae8e8'}}>
       {/* Affichage de l'iframe publicitaire */}
+      {/* font-size:11px;padding-top:18px; */}
+      <center>
+      <p style={{fontSize:11, paddingTop:18}}>LA SUITE APRÈS LA PUBLICITÉ</p>        
       <iframe
         id="a5699f99"
         name="a5699f99"
         src={`https://client.cynomedia-africa.com/www/delivery/afr.php?zoneid=208&cb=${Date.now()}`}
         frameBorder="0"
         scrolling="no"
-        width="336"
-        height="280"
+        width='100%' 
+        height='250'
         allow="autoplay"
         style={{ display: "block", margin: "0 auto" }}
       ></iframe>
+      </center>
+      <br/><br/>
     </div>
   );
 };
