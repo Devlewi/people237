444c1e1a35306e9c8949789ce0a317da5176df42
devlewi <lewimarcjean3@gmail.com>
2025-03-07 08:30:31 +0000

    desactivation de l'optimisation des images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {    
+    unoptimized: true,//Désactiver l'optimisation d'images
+    remotePatterns: [      
+      {
+        protocol: "https",
+        hostname: "localhost",
+        pathname: "/wp-content/uploads/**",
+      },
+      {
+        protocol: "https",
+        hostname: "controlpanel.people237.com",
+        pathname: "/wp-content/uploads/**",
+      },        
+    ],
     domains: ['people237.com','controlpanel.people237.com','people237-frontend.vercel.app', 'people237.vercel.app','client.cynomedia-africa.com'], // Ajoutez ici le domaine de votre image
   },
   
--- src/app/components/Banner336x280.tsx
+++ src/app/components/Banner336x280.tsx
@@ -1,30 +1,49 @@
 "use client";
 
+import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
 const Banner336x280: React.FC = () => {
   const [isClient, setIsClient] = useState(false);
+  const [randomNumber, setRandomNumber] = useState<number>(Math.floor(Math.random() * 1000000));
 
   useEffect(() => {
     setIsClient(true);
   }, []);
 
+  useEffect(() => {
+    // Générer un nouveau numéro aléatoire toutes les 30 secondes (si nécessaire)
+    const interval = setInterval(() => {
+      setRandomNumber(Math.floor(Math.random() * 1000000));
+    }, 30000);
+
+    return () => clearInterval(interval); // Cleanup
+  }, []);
+
   if (!isClient) return null; // Empêche le rendu côté serveur
-//text-align:center;background:#eae8e8;margin-top:0px;
+
   return (
-    <div style={{textAlign:'center'}}>
+    <div style={{ textAlign: "center", marginTop: "0px" }}>
       {/* Affichage de l'iframe publicitaire */}
       <iframe
-        id="a5699f99"
-        name="a5699f99"
-        src={`https://client.cynomedia-africa.com/www/delivery/afr.php?zoneid=208&cb=${Date.now()}`}
+        id="afb1865f"
+        name="afb1865f"
+        src={`https://client.cynomedia-africa.com/www/delivery/afr.php?zoneid=208&cb=${randomNumber}`}
         frameBorder="0"
         scrolling="no"
         width="336"
         height="280"
         allow="autoplay"
-        style={{ display: "block", margin: "0 auto" }}
-      ></iframe>
+      >
+        <a href={`https://client.cynomedia-africa.com/www/delivery/ck.php?n=a5c548db&cb=${randomNumber}`} target="_blank" rel="noopener noreferrer">
+          <Image
+            src={`https://client.cynomedia-africa.com/www/delivery/avw.php?zoneid=208&cb=${randomNumber}&n=a5c548db`}
+            width={336}
+            height={280}
+            alt="Advertisement"
+          />
+        </a>
+      </iframe>
     </div>
   );
 };
